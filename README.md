e4621aedbcd6aee9373678c87ef6852aa34fedb0
atif2843 <muhammadatif93@gmail.com>
2020-08-06 22:20:57 +0530

    Titu home changes

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # FireLock-v0.01
+
+# [✔] Header
+# [✔] index page
+# [✔] product nav page
+# [] product nav slide images
+# [✔] product nav slide
+# [✔] product nav links 
+# [✔] product pages 
+# [✔] product page slide
+# [✔] pro
---
+++ script/productnav_script.js
@@ -0,0 +1,47 @@
+const slider = document.querySelector(".items");
+		const slides = document.querySelectorAll(".item");
+		const button = document.querySelectorAll(".button");
+        const text=document.querySelectorAll(".text");
+
+		let current = 0;
+		let prev = 3;
+		let next = 1;
+
+		for (let i = 0; i < button.length; i++) {
+                button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
+		}
+
+		const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
+
+		const gotoNext = () => current < 3 ? gotoNum(current + 1) : gotoNum(0);
+
+		const gotoNum = number => {
+			current = number;
+			prev = current - 1;
+			next = current + 1;
+
+			for (let i = 0; i < slides.length; i++) {
+				slides[i].classList.remove("active");
+				slides[i].classList.remove("prev");
+                slides[i].classList.remove("next");
+			}
+      for (let i=0; i<text.length; i++){
+        text[i].classList.remove("active");
+
+      }
+
+			if (next == 4) {
+				next = 0;
+			}
+
+			if (prev == -1) {
+				prev = 3;
+			}
+
+			slides[current].classList.add("active");
+			slides[prev].classList.add("prev");
+			slides[next].classList.add("next");
+            text[current].classList.add("active");
+        }
+        console.log(text);
+        console.log(slides);
